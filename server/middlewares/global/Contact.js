d0602fac38959da0a1238f4327bd5e6e3c0fe49e
Thinakaran <thinakaranmanohar@yahoo.com>
2025-05-03 21:50:52 +0530

    Finished

--- server/middlewares/global/Contact.js
+++
@@ -1,9 +0,0 @@
-exports.ContactRequest = async (req, res, next) => {
-    const { name, email, message } = req.body;
-
-    if (!name || !email || !message) {
-        return res.status(400).json({ message: 'All required fields must be filled' });
-    }
-
-    next();
-}
--- server/models/global/Contact.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Contact = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    message: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model("Contact", Contact);
